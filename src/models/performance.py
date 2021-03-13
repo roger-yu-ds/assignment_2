c0110de2d2c2feafb36fccfb5796dcd22d7d6d00
Roger Yu <rogeryu@hotmail.com.au>
2021-03-13 16:28:23 +1100

    Add functions to `performance.py`
    * `convert_cr_to_dataframe`
    * `roc_auc_score_multiclass`

--- src/models/performance.py
+++ src/models/performance.py
@@ -1,43 +1,57 @@
 from sklearn.metrics import roc_auc_score
+from typing import Dict
+import pandas as pd
 
 
-def print_reg_perf(y_preds, y_actuals, set_name=None):
-    """Print the RMSE and MAE for the provided data
-
-    Parameters
-    ----------
-    y_preds : Numpy Array
-        Predicted target
-    y_actuals : Numpy Array
-        Actual target
-    set_name : str
-        Name of the set to be printed
-
-    Returns
-    -------
+def convert_cr_to_dataframe(report_dict: Dict) -> pd.DataFrame:
     """
-    from sklearn.metrics import mean_squared_error as mse
-    from sklearn.metrics import mean_absolute_error as mae
+    Converts the dictionary format of the Classification Report (CR) to a
+    dataframe for easy of sorting
+    :param report_dict: The dictionary returned by
+    sklearn.metrics.classification_report.
+    :return: Returns a dataframe of the same information.
+    """
+    beer_style = list(report_dict.keys())
+    beer_style.remove('accuracy')
+    beer_style.remove('macro avg')
+    beer_style.remove('weighted avg')
 
-    print(f"RMSE {set_name}: {mse(y_actuals, y_preds, squared=False)}")
-    print(f"MAE {set_name}: {mae(y_actuals, y_preds)}")
+    precision = []
+    recall = []
+    f1 = []
+    support = []
+
+    for key, value in report_dict.items():
+        if key not in ['accuracy', 'macro avg', 'weighted avg']:
+            precision.append(value['precision'])
+            recall.append(value['recall'])
+            f1.append(value['f1-score'])
+            support.append(value['support'])
+
+    result = pd.DataFrame({'beer_style': beer_style,
+                           'precision': precision,
+                           'recall': recall,
+                           'f1': f1,
+                           'support': support})
+
+    return result
 
 
-def roc_auc_score_multiclass(actual_class, pred_class, average = "macro"):
+def roc_auc_score_multiclass(actual_class, pred_class, average="macro"):
     # From https://stackoverflow.com/a/52750599
-    #creating a set of all the unique classes using the actual class list
+    # creating a set of all the unique classes using the actual class list
     unique_class = set(actual_class)
     roc_auc_dict = {}
     for per_class in unique_class:
-    #creating a list of all the classes except the current class
-    other_class = [x for x in unique_class if x != per_class]
+        # creating a list of all the classes except the current class
+        other_class = [x for x in unique_class if x != per_class]
 
-    #marking the current class as 1 and all other classes as 0
+    # marking the current class as 1 and all other classes as 0
     new_actual_class = [0 if x in other_class else 1 for x in actual_class]
     new_pred_class = [0 if x in other_class else 1 for x in pred_class]
 
-    #using the sklearn metrics method to calculate the roc_auc_score
-    roc_auc = roc_auc_score(new_actual_class, new_pred_class, average = average)
+    # using the sklearn metrics method to calculate the roc_auc_score
+    roc_auc = roc_auc_score(new_actual_class, new_pred_class, average=average)
     roc_auc_dict[per_class] = roc_auc
 
     return roc_auc_dict
